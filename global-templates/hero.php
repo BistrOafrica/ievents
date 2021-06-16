009fd24b476e8179a55be971f1c7d175b4715592
BistrOafrica <35723096+BistrOafrica@users.noreply.github.com>
2021-06-17 00:40:48 +0300

    Added a default Hero image
    All pages without a background image or a Hero image will display this image by default.

--- global-templates/hero.php
+++ global-templates/hero.php
@@ -11,8 +11,18 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 ?>
 </div>
+	<?php
+		$heroImgUri= get_the_post_thumbnail_url();
+		$defaultHeroUri=get_template_directory_uri().'/img/defaultHero.jpg';
+
+		if($heroImgUri!=""){
+			$heroUri=$heroImgUri;
+		}else{
+			$heroUri=$defaultHeroUri;
+		}
+	?>
 <div class="page-header" style="background: url('<?php  
-				echo get_the_post_thumbnail_url();
+				echo $heroUri;
 				?>'); background-size: cover; background-repeat: no-repeat; background-position-y: center; min-height:500px">
                 </div>
 <div class="<?php echo esc_attr( $container ); ?>" id="wrapper-static-content" tabindex="-1">
