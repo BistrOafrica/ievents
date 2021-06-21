d272f7bafd0632fd8a70cbf11c56802b3e20a1cb
BistrOafrica <35723096+BistrOafrica@users.noreply.github.com>
2021-06-21 20:34:58 +0300

    Default Hero on Page Template.

--- page.php
+++ page.php
@@ -16,13 +16,21 @@ defined( 'ABSPATH' ) || exit;
 get_header();
 
 $container = get_theme_mod( 'understrap_container_type' );
+$heroImgUri= get_the_post_thumbnail_url();
+		$defaultHeroUri=get_template_directory_uri().'/img/defaultHero.jpg';
 
+		if($heroImgUri!=""){
+			$heroUri=$heroImgUri;
+		}else{
+			$heroUri=$defaultHeroUri;
+		}
 ?>
 
 <div class="wrapper" id="page-wrapper">
 </div>
+
 <div class="page-header" style="background: url('<?php  
-				echo get_the_post_thumbnail_url();
+				echo $heroUri;
 				?>'); background-size: cover; background-repeat: no-repeat; background-position-y: center; min-height:500px">
                 </div><br/>
 <div class="<?php echo esc_attr( $container ); ?>" id="wrapper-static-content" tabindex="-1">
